afacba0c5a8f862209196506ff2b4fa22209f714
ghaliaghazal <ghalia.ghazal2009@gmail.com>
2026-08-31 23:50:32 +0200

    Add solar and lunar API endpoints

--- backend/src/backend/api.py
+++ backend/src/backend/api.py
@@ -1,4 +1,13 @@
 from fastapi import FastAPI
+from backend.data_processing import solar_df, lunar_df
 
 
 app = FastAPI()
+
+@app.get("/solar")
+async def show_solar_data():
+    return solar_df
+
+@app.get("/lunar")
+async def show_lunar_data():
+    return lunar_df
