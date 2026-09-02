e2a7a4ba6fd5f865cf39dd7a1548ef6b80d6ff58
ghaliaghazal <ghalia.ghazal2009@gmail.com>
2026-09-02 16:05:47 +0200

    Add eclipse charts and dashboard updates

--- frontend/src/frontend/dashboard.py
+++ frontend/src/frontend/dashboard.py
@@ -1,20 +1,55 @@
 
 import streamlit as st 
 import httpx 
-
+import pandas as pd
 
 
 BASE_URL = "http://127.0.0.1:8000" 
 
 def main(): 
-    st.markdown("# EclipseBord") 
+    st.markdown("## EclipseBord")
+    st.write("Explore solar and lunar eclipse data.")
 
     solar_data = httpx.get(f"{BASE_URL}/solar", timeout=30).json() 
     lunar_data = httpx.get(f"{BASE_URL}/lunar", timeout=30).json() 
 
-    st.dataframe(solar_data) 
-    st.dataframe(lunar_data) 
+    
+    solar_df = pd.DataFrame(solar_data) # Convert eclipse data to DataFrames
+    lunar_df = pd.DataFrame(lunar_data) 
 
+
+    # Add eclipse record metrics
+    col1, col2 = st.columns(2)
+
+    with col1:
+        st.metric("Number of solar eclipse records", len(solar_data))
+
+    with col2:
+        st.metric("Number of lunar eclipse records", len(lunar_data))
+
+
+    # Add tabs for solar and lunar eclipse data
+    solar_tab, lunar_tab = st.tabs(
+    ["☀️ Solar Eclipses", "🌙 Lunar Eclipses"]
+    )
+    
+    with solar_tab:
+        st.dataframe(solar_data)
+
+    with lunar_tab:
+        st.dataframe(lunar_data)
+
+    
+
+    # Add solar eclipse types chart:
+    solar_types = solar_df["Eclipse Type"].value_counts()
+    st.subheader("Solar Eclipse Types")
+    st.bar_chart(solar_types)
+
+    # Add lunar eclipse types chart:
+    lunar_types = lunar_df["Eclipse Type"].value_counts()
+    st.subheader("Lunar Eclipse Types")
+    st.bar_chart(lunar_types)
 
 if __name__ == "__main__": 
     main()
